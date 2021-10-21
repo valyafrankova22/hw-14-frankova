31d070f54edc1c55866b393d00a7beccf02d57b8
valyafrankova22 <vfrankova22@icloud.com>
2021-10-21 21:44:45 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function padString (str, strLenght, symb, right = true) {
    if (typeof str !== `string`) return `str is not string`;
    if (typeof strLenght !== `number` && isNaN(strLenght)) return `strLenght is not number`;
-   if (typeof symb !== `string` && symb !== 1) return `symb not valid`;
+   if (!symb || (typeof symb !== `string` && symb !== 1)) return `symb not valid`;
    if (typeof right !== "boolean") return `right is invalid`;
    if (str.length >= strLenght) return str.substring(0, strLenght);
 
